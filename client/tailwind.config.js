39731375c5b7ed7cbaf5ee2af051ae5aae0530fb
dawudarif <dawuddarif@gmail.com>
2023-12-19 13:01:30 +0500

    initial commit

---
+++ client/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      screens: {
+        xs: { max: "540px" },
+        sm: { min: "541px", max: "767px" },
+        md: { min: "768px", max: "1220px" },
+        lg: { min: "1221px" },
+      },
+    },
+  },
+  plugins: [],
+};
