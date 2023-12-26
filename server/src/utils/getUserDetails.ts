686dc74cc0e9db2ef8f3b2c5e43f66ff0bded390
dawudarif <dawuddarif@gmail.com>
2023-12-26 21:45:31 +0500

    fix: auth middleware

--- server/src/utils/getUserDetails.ts
+++ server/src/utils/getUserDetails.ts
@@ -4,32 +4,38 @@ import { prisma } from '../prisma/prisma';
 
 
 export const getUserDetails = async (token: string) => {
-  const cookies = token.split('; ');
-  const jwtCookie = cookies.find((cookie: string) => cookie.startsWith('jwt='));
+  try {
+    const cookies = token.split('; ');
+    const jwtCookie = cookies.find((cookie: string) => cookie.startsWith('jwt='));
 
-  if (token && process.env.JWT_SECRET) {
-    if (jwtCookie) {
-      const jwtValue = jwtCookie.split('=')[1];
-      token = jwtValue;
+    if (token && process.env.JWT_SECRET) {
+      if (jwtCookie) {
+        const jwtValue = jwtCookie.split('=')[1];
+        token = jwtValue;
+      }
+
+      const decoded = jwt.verify(token, process.env.JWT_SECRET) as IDecoded;
+
+
+      const findUser = await prisma.account.findUnique({
+        where: {
+          id: decoded?.userId,
+        },
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          createdAt: true,
+          username: true,
+        },
+      });
+
+
+      return findUser
     }
 
-    const decoded = jwt.verify(token, process.env.JWT_SECRET) as IDecoded;
+  } catch (error) {
 
-
-    const findUser = await prisma.account.findUnique({
-      where: {
-        id: decoded?.userId,
-      },
-      select: {
-        id: true,
-        name: true,
-        email: true,
-        createdAt: true,
-        username: true,
-      },
-    });
-
-
-    return findUser
   }
+
 }
